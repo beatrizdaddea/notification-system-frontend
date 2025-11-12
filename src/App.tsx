ede76b9c09b4389189cb6543bc64f07995a660b9
beatrizdaddea <beatrizchith@gmail.com>
2025-11-12 10:49:25 -0300

    feat: setup notification SPA in react project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { NotificationsPage } from "./pages/NotificationsPage";
+
+const App: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <NotificationsPage />
+    </div>
+  );
+};
+
+export default App;
